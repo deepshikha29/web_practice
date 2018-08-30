a18d22c6c5a4e6d733dd9a13a7a269d3a9a71a2f
Bhatt <Deepshikha.Bhatt@aristocrat.com>
2018-08-30 17:48:22 +0530

    isolated script file as Calsulator.js

---
+++ Calculator.js
@@ -0,0 +1,61 @@
+var inputBtns = document.getElementsByClassName('inputBtn');
+        console.log(inputBtns);
+        var lengthOfInputButtons = inputBtns.length;
+        var clickOperator = document.getElementsByClassName('operator');
+        var BeingUsedOperator;
+        var IsOperatorPresent = false;
+        
+
+        
+            for (i = 0; i < lengthOfInputButtons; i++) {
+                inputBtns[i].onclick = function () {
+
+                    document.getElementById('InputBox').value += this.getAttribute("name");
+                    var num = document.getElementById('InputBox').value;
+
+                    console.log("number:" + num);
+
+                }
+            }
+        
+        
+        for (j = 0; j < clickOperator.length; j++) {
+            clickOperator[j].onclick = function () {
+                if (IsOperatorPresent==false){
+                document.getElementById('InputBox').value += this.getAttribute("name");
+                BeingUsedOperator = this.getAttribute("name");
+                IsOperatorPresent = true;
+                console.log(currentOperator);
+                }
+                else{
+                    array=document.getElementById("InputBox").value.split(BeingUsedOperator);
+
+                    if (BeingUsedOperator=="+")
+                    {
+                    var output = Number(array[0])+Number(array[1]);
+                    }
+                    if (BeingUsedOperator=="-")
+                    {
+                    var output = Number(array[0])-Number(array[1]);
+                    }
+                    if (BeingUsedOperator=="*")
+                    {
+                    var output = Number(array[0])*Number(array[1]);
+                    }
+                    if (BeingUsedOperator=="/")
+                    {
+                    var output = Number(array[0])/Number(array[1]);
+                    }
+                    if (this.getAttribute("name")=="=")
+                    {
+                        document.getElementById('InputBox').value = output;
+                    }
+                    else{
+                    document.getElementById('InputBox').value = output + this.getAttribute("name");
+                    BeingUsedOperator = this.getAttribute("name");
+                    console.log(output);
+                    }
+                }
+
+            }
+        }
